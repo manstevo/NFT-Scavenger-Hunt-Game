6e51d686ad0ea549708ddd051c219f617d7a6a16
Falade Yusuf <faladeyusuf54@gmail.com>
2025-01-16 14:48:26 +0100

    removed the redirects file to test if it will still work in netlify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Necessary for deploying to Netlify
-  // trailingSlash: true,
 };
 
 export default nextConfig;
