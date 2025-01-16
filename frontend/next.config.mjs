35aa4754ded6e6772acdde53895555c5c452360b
Falade Yusuf <faladeyusuf54@gmail.com>
2025-01-16 13:18:55 +0100

    added a redirects for netlify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone", // Necessary for deploying to Netlify
+  trailingSlash: true,
 };
 
 export default nextConfig;
