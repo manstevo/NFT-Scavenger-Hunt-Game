32c8befcc24b7a9a86055623785cd1427f422973
Falade Yusuf <faladeyusuf54@gmail.com>
2025-01-16 12:53:14 +0100

    test

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "standalone", // Necessary for deploying to Netlify
 };
 
 export default nextConfig;
