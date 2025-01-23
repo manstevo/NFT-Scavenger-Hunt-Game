150bf5d9fb44ee59753dc54050a5f10da4b91f69
Uyoxy <blessingtimothy20171@gmail.com>
2025-01-23 13:26:12 +0100

    implement pages

---
+++ frontend/app/puzzle/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Puzzle = () => {
+  return (
+    <div>
+      puzzle page
+    </div>
+  )
+}
+
+export default Puzzle
---
+++ frontend/app/rewards/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Reward = () => {
+  return (
+    <div>
+      reward page
+    </div>
+  )
+}
+
+export default Reward
