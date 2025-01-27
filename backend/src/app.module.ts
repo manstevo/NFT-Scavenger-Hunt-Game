4f152ac5653a620c0636b71fdc13fe1947f08d17
OthmanImam <imkargi@gmail.com>
2025-01-26 16:05:41 -0800

    uncommented typeOrmModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -21,21 +21,21 @@ import databaseConfig from 'config/database.config';
       load: [appConfig, databaseConfig],
       cache: true
     }),
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: (configService: ConfigService) => ({
-    //     type: 'postgres',
-    //     host: configService.get('database.host'),
-    //     port: +configService.get('database.port'),
-    //     username: configService.get('database.user'),
-    //     password: configService.get('database.password'),
-    //     database: configService.get('database.name'),
-    //     blog: configService.get('database.blog'),
-    //     synchronize: configService.get('database.synchronize'),
-    //     autoLoadEntities: configService.get('database.autoload')
-    //   }),
-    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        blog: configService.get('database.blog'),
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: configService.get('database.autoload')
+      }),
+    }),
     UsersModule,
     PuzzlesModule,
     NftsModule,
